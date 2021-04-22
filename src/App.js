4d73ea6d8592b5879c8b75c4e35c7e8e52e53d10
LenaIsLove <k.shkryob@gmail.com>
2021-04-22 22:19:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Zoom from 'react-reveal/Zoom';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/icon/favicon-96x96.png"/>
+      <Zoom>
+        <p>Лена</p>
+      </Zoom>
+      <Zoom>
+        <p>моя</p>
+      </Zoom>
+      <Zoom>
+        <p>ЛЮБИМА</p>
+      </Zoom>
+      <Zoom>
+        <p><span class="biruzoviy">жопця</span></p>
+      </Zoom>
+    </div>
+  );
+}
+
+export default App;
